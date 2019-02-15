67c4d84d373dafe67b06a0b1e82014b45a9b45d4
Ivonne Yanez Mendoza <ivonne.wftm@gmail.com>
2019-02-15 09:49:14 +0100

    add json

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ var TriviaGame = function (questions) {
     this.answer = []; //save choose answer
     this.correctAnswer = 0; // save correct
     this.wrongAnswer = 0; // save incorrect
-    this.unanswer = null; //save unanswer
-    this.timeLeft = 20; // use for countdown
+    this.unanswer = 0; //save unanswer
+    this.timeLeft = 10; // use for countdown
     this.id = 0;
-    this.running = false;
-
-    
-    
 }
+
+
 TriviaGame.prototype.shuffle = function () {
     var array= this.questions;
     for (let i = array.length - 1; i > 0; i--) {
@@ -22,47 +20,49 @@ TriviaGame.prototype.shuffle = function () {
 
 TriviaGame.prototype.downCounter = function(){
 /*var*/ this.id = setInterval( () => {
-    if(this.timeLeft == 0){
+
+    if(this.timeLeft === 0){
         clearInterval(this.id);
         this.unanswer++
+        console.log("unanser question " + this.unanswer)
+        return this.unanswer;
+        
+    
     }else{
         this.timeLeft --
         $("#countdown").text(this.timeLeft);
-        // return this.timeLeft;
+        return this.timeLeft;
     }
 }, 1000);
-
 
 
 }
 
 
-TriviaGame.prototype.gameResult = function(choice, correct){
-  
 
-    // if(choice.length == 0 || choice.length == undefined) {
-    //     this.unanswer ++
-    //     console.log("no answer")
-    //     return false;
-        
-    // }
+TriviaGame.prototype.prueba = function () {
+    if(this.unanswer > 0){
+        console.log(this.unanswer)
+    }
+    
+}
+
+
+TriviaGame.prototype.gameResult = function(choice, correct){
+
     if(choice == correct){
         this.correctAnswer ++
         return true;
-        
     }else {
         this.wrongAnswer ++
         return false 
     }
-  
-
 }
 
 TriviaGame.prototype.stop = function(){
     
     clearInterval(this.id);
     
-
 }
 
 TriviaGame.prototype.finishedGame = function(){
--- src/main.js
+++ src/main.js
@@ -4,38 +4,30 @@ var questions = [
         alternatives: [ "Creep: Radiohead", "House of rising sun: The animals","Billie Jean: Michael Jackson", "Get lucky: Daft Punk"],
         correct:      [1, "House of rising sun: The Animals"],
         photo:          "images/animals.gif"
-        
-   
     },
 
     {   option:      "Which movie was won an Oscar for best movie song",
         alternatives: [ "Working girl: 1987","The body guard: 1992","Wall-e: 2008 ","127 hours 2010 "],
         correct:      [0, "Working Girl 1987"],
         photo:         "images/working.gif"
-        
     },
     
     {   option:      "Terminator 2 includes a rock song from",
         alternatives: [ "Guns of roses: Sweet child of mine","Poison: Talk dirty to me","Black Sabbath: Brat song", "Guns of roses: You could be mine "],
         correct:      [ 3, "You could be mine"],
         photo:          "images/terminator.gif",
-        
-      
     },
 
     {   option:      "Which country won Eurovision 1974?",
         alternatives: [ "Austria","Netherlands","Sweden","Denmark",],
         correct:       [ 2, "Sweden"],
         photo:          "images/abba.gif",
-        
     },
 
-    {
-        option:      "Which album won the best album in 2016 grammys?",
+    {   option:      "Which album won the best album in 2016 grammys?",
         alternatives:  [ "Ed Sheeran: Whatever", "Taylor Swift: 1989", "Kendrick Lamar: Alright", "Radiohead: A moon shapel pool"],
         correct:       [ 1, "Taylor Swift 1989"],
         photo:          "images/taylorsad.gif",
-        
     },
 ]
 
@@ -71,106 +63,73 @@ $("#music-movies").click(function(){
         $(this).hide()
         $("iframe").show();
         
-       
-       
         //display down counter
         triviaGame.downCounter()
-        displayQuestion(triviaGame.answer.length);
         
+        displayQuestion(triviaGame.answer.length);
     });     
 })
+
 
 function displayQuestion(numberOfQuestion) {
 
 
     $("#question-block").html((triviaGame.questions[numberOfQuestion].option));
-    
-
     triviaGame.questions[numberOfQuestion].alternatives.forEach((element, index) => {
-        // console.log(index)
-        $("#answer-block").append(`<li data-index=${index}> ${element} </li>`)
+    $("#answer-block").append(`<li data-index=${index}> ${element} </li>`)
       
     });
 
+    
+
     $("li").click(function(){
-       
         triviaGame.answer.push($(this).data("index"));                       
         var choice = $(this).data("index"); // save gamer choice
         var correct = questions[numberOfQuestion].correct[0]; //save correct answer
       
-        if(triviaGame.gameResult(choice, correct)){
-            
-            triviaGame.stop();
-            $("#answer-block").hide();
-            $("#btnHint").hide();
-            $("#answer-block").show().text("CORRECT!! You are awesome");
-            $("#correctqs").text("Correct answers " + triviaGame.correctAnswer);
-            
-
-        } else {
-            triviaGame.stop()
-            $("#answer-block").hide();
-            $("#btnHint").hide();
-            //show a correct answer
-            $("#answer-block").show().text("You lose! :( the correct answer is " + questions[numberOfQuestion].correct[1]);
-            $("#image").html("<img src =" + triviaGame.questions[numberOfQuestion].photo+ ">");
-            $("#incorrectqs").text("Incorrect answers " + triviaGame.wrongAnswer);
-        }
-     
-        triviaGame.finishedGame();
-    
-    setTimeout(()=>{ 
-        $("#answer-block").empty();
-        $("#image").empty();
-        triviaGame.reset();
-        triviaGame.unanswer;
-        $("#countdown").text(triviaGame.timeLeft);
-        triviaGame.downCounter();
-        displayQuestion(triviaGame.answer.length);
+            if(triviaGame.gameResult(choice, correct)){
+                triviaGame.stop();
+                $("#answer-block").hide();
+                $("#btnHint").hide();
+                $("#answer-block").show().text("CORRECT!! You are awesome");
+                $("#correctqs").text("Correct answers " + triviaGame.correctAnswer);
                 
-    },5000)
 
-    $("#btnReset").click(function(){
-        location.reload();
-    })
-
-
-
-
-
-
-});
+            } else {
+                triviaGame.stop()
+                $("#answer-block").hide();
+                $("#btnHint").hide();
+                //show a correct answer
+                $("#answer-block").show().text("You lose! :( the correct answer is " + questions[numberOfQuestion].correct[1]);
+                $("#image").html("<img src =" + triviaGame.questions[numberOfQuestion].photo+ ">");
+                $("#incorrectqs").text("Incorrect answers " + triviaGame.wrongAnswer);
+            }
+     
+            triviaGame.finishedGame();
     
+                setTimeout(()=>{ 
+                $("#answer-block").empty();
+                $("#image").empty();
+                triviaGame.reset();
+                triviaGame.unanswer;
+                $("#countdown").text(triviaGame.timeLeft);
+                triviaGame.downCounter();
+                displayQuestion(triviaGame.answer.length);
+                    
+                },5000)
 
-        
-
+        $("#btnReset").click(function(){
+            location.reload();
+        })
+    });
 }
 
 
 
 
-/*******************************/
-//Hint//
-/*******************************/
 
-        // set button 50/50 hint
-        // necesito entregar pistas:
-        $("#btnHint").click(function(){
-            var hintbtn = new TriviaGame(questions);
-            // 0) defino respuesta correcta
-            let answer = hintbtn.questions[0].correct; //respuestas correctas
-            let result = hintbtn.questions[0].alternatives[answer]; //preguntas
- 
-            //1) elimino respuesta correcta desde arreglo
 
-            
-            // 2) creo un alert que entregue el hint en base a paso anterior
 
-            alert(`El resultado es: ${result}`);
-            
- 
-           
-        });
 
 
 
